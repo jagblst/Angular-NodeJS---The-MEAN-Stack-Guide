2c94051f4d26c9bc54139662994d5327692655f8
Jaga <jagblst@gmail.com>
2024-02-14 21:09:45 +0200

    Angular FrontEnd Basics
    Creating a Post Model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { Post } from './posts/post.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  storedPosts = [];
+  storedPosts: Post[] = [];
   onPostAdded(post) {
     this.storedPosts.push(post);
   }
