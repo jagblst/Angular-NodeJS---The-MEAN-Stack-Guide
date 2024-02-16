e32f5b5e3a8b92d8ebaac56eea659be7619d9eab
Jaga <jagblst@gmail.com>
2024-02-16 12:37:46 +0200

    Angular FrontEnd + Node Backend
    Deleting Documents

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, DELETE, OPTIONS"
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
   next();
 });
@@ -37,7 +37,7 @@ app.post("/api/posts", (req, res, next) => {
   });
 });
 
-app.use("/api/posts", (req, res, next) => {
+app.get("/api/posts", (req, res, next) => {
   Post.find()
     .then((documents) => {
       res
@@ -45,6 +45,13 @@ app.use("/api/posts", (req, res, next) => {
         .json({ message: "Post fetched succesfully!", posts: documents });
     })
     .catch(() => console.log("error"));
+});
+
+app.delete("/api/posts/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then((result) => {
+    console.log(result);
+  });
+  res.status(200).json({ message: "Post deleted!" });
 });
 
 module.exports = app;
