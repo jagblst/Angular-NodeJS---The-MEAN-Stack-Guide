8ba356590e429c0e23ebad160a1113ffc7cad38b
Jaga <jagblst@gmail.com>
2024-02-16 13:15:21 +0200

    Angular FrontEnd + Node Backend
    Adding Posts with an ID

--- backend/app.js
+++ backend/app.js
@@ -31,9 +31,11 @@ app.use((req, res, next) => {
 
 app.post("/api/posts", (req, res, next) => {
   const post = new Post({ title: req.body.title, content: req.body.content });
-  post.save();
-  res.status(201).json({
-    message: "Post added succesfully.",
+  post.save().then((createdPost) => {
+    res.status(201).json({
+      message: "Post added succesfully.",
+      postId: createdPost._id,
+    });
   });
 });
 
