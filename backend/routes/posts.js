91fb98cfb240a34681081b3d26eec15934b1a128
Jaga <jagblst@gmail.com>
2024-02-19 20:07:58 +0200

    Adding Image Uploads
    Working with the File URL

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -31,11 +31,21 @@ router.post(
   "",
   multer({ storage: storage }).single("image"),
   (req, res, next) => {
-    const post = new Post({ title: req.body.title, content: req.body.content });
+    const url = req.protocol + "://" + req.get("host");
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: url + "/images/" + req.file.filename,
+    });
     post.save().then((createdPost) => {
       res.status(201).json({
         message: "Post added succesfully.",
-        postId: createdPost._id,
+        post: {
+          id: createdPost._id,
+          title: createdPost.title,
+          content: createdPost.content,
+          imagePath: createdPost.imagePath,
+        },
       });
     });
   }
