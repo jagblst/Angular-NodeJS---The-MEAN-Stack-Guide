082229b7ca5703b7ce6eb7011e5ba6ecdc1de5c4
Jaga <jagblst@gmail.com>
2024-02-19 20:22:54 +0200

    Adding Image Uploads
    Fetching Images on the Frontend

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -15,6 +16,7 @@ mongoose
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended:false})); -example
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
